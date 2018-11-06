c2fd0a66057c1ebc1fbbbfdb5050e584284e53b4
peyrash <43827651+peyrash@users.noreply.github.com>
2018-11-05 20:04:19 -0600

    Delete Regression_MissingDataN.py

--- Regression_MissingDataN.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
-import statsmodels.formula.api as smf
-df=pd.read_csv("C:\\Users\\peyma\\PycharmProjects\\HW2\\celeb.csv",encoding="cp1252")
-df.head()
-df = df.replace(r'^\s+$', np.nan, regex=True)
-df=df.dropna(axis=0, how='any')
